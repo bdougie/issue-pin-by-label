a1cbfbe31f32b43e070c6a3303607243f608266d
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-06-18 13:21:32 -0700

    action successfully pins

--- index.js
+++ index.js
@@ -1,77 +1,54 @@
 const { Toolkit } = require('actions-toolkit')
+//
+// pinnedIssues is still in developer preview
+const headers = {"Accept": "application/vnd.github.elektra-preview"}
 
-const query = `query {
-  repository (owner: "github", name: "developer-relations") {
-    pinnedIssues (first: 3) {
-      totalCount
-      nodes {
-        issue {
-          title
-          number
-          labels (first: 100) {
-            nodes {
-              name
-            }
-          }
-        }
-      }
+const mutation = `mutation ($input: PinIssueInput!) {
+  pinIssue(input: $input) {
+    issue {
+      title
     }
   }
 }`
 
-// const query = `query ($owner: String!, $repo: String!) {
-//   repository (owner: $owner, name: $repo) {
-//     pinnedIssues (first: 3) {
-//       totalCount
-//       nodes {
-//         issue {
-//           title
-//           number
-//           labels (first: 100) {
-//             nodes {
-//               name
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }`
-
-const vairables = ""
+async function pinIssue (tools, id) {
+  const input = { issueId: id, clientMutationId: "top5 pinned" } 
+  const variables = {input: input, headers: headers}
+  return tools.github.graphql(mutation, variables)
+}
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
-  // Create the new issue
+  const action = tools.context.payload.action
 
-  // return results.repository.pinnedIssues.nodes
-  const headers = {"Accept": "application/vnd.github.elektra-preview+json"}
-  // const results = await tools.github.graphql(query("github", "developer-relations"), {headers})
-  const results = await tools.github.graphql(query, {headers})
-
-  tools.log.success(results)
+  if (action.indexOf("labeled" === -1)) {
+    tools.exit.neutral("Just checking for recent labels")
+  }
 
   try {
-     // const issue = await tools.github.issues.create({
-     //   assignees: listToArray(attributes.assignees),
-     //   labels: listToArray(attributes.labels),
-     //   milestone: attributes.milestone
-     // })
+    const label = tools.context.payload.label.name
 
-    tools.log.success(tools.context)
-    // tools.log.success(`Labeled issue ${issue.data.title}#${issue.data.number}: ${issue.data.html_url}`)
+    // pin the issue when labeled
+    if (action.indexOf("labeled" === 0 && label === "top5")) {
+      const issue = tools.context.payload.issue
+      const results = await pinIssue(tools, issue.node_id)
+
+      tools.log.success(`Issue #${issue.title} pinned`)
+      tools.exit.success("Action is complete")
+    }
+
+    tools.exit.success('Issue labeled with non-pinnable label')
   } catch (err) {
     // Log the error message
-    tools.log.error(`An error occurred while pinning the issue. This might be caused by a malformed issue title, or a typo in the labels`)
+    tools.log.error(`An error occurred while pinning the issue.`)
     tools.log.error(err)
 
     // The error might have more details
     if (err.errors) tools.log.error(err.errors)
 
     // Exit with a failing status
-    // tools.exit.failure()
+    tools.exit.failure()
   }
-  // tools.exit.success('We did it!')
 }, {
   secrets: ['GITHUB_TOKEN']
 })
