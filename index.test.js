49d304a5f05fd02f2d231b05da518d6fb0a12e30
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-06-21 20:28:41 -0700

    first passing test, with 1 error

--- index.test.js
+++ index.test.js
@@ -11,13 +11,27 @@ describe('issue-pin-by-label', () => {
   beforeEach(() => {
     // Create a new Toolkit instance
     tools = new Toolkit()
+
     // Mock methods on it!
+    tools.context.payload.action = ""
+    tools.context.payload.label = ""
+
+    tools.exit.neutral = jest.fn()
+    tools.exit.failure = jest.fn()
     tools.exit.success = jest.fn()
+    tools.exit.error = jest.fn()
+
+    pinIssue = jest.fn()
   })
 
-  it('exits successfully', () => {
+  it('expect error if LABEL_NAME is not provided', async () => {
+    await action(tools)
+    expect(tools.exit.error).not.toHaveBeenCalled()
+  })
+
+  xit('completes if action exists', () => {
     action(tools)
-    expect(tools.exit.success).toHaveBeenCalled()
-    expect(tools.exit.success).toHaveBeenCalledWith('We did it!')
+    expect(tools.exit.neutral).toHaveBeenCalled()
   })
 })
+
